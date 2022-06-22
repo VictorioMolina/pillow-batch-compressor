4b3ce2079afb9c37d28f380b388fb35595990509
Victorio Molina <54834743+VictorioMolina@users.noreply.github.com>
2022-06-22 13:00:57 +0200

    feature: resizing images applying a scale factor

--- compressor.py
+++ compressor.py
@@ -13,42 +13,48 @@ from PIL import Image
 """
 Compress an image applying the given quality.
 """
-def compress_image(image_path, compressed_image_path, quality):
+def compress_image(image_path, compressed_image_path, quality, scale_factor):
     print(f'> Compressing {image_path}...');
     picture = Image.open(image_path);
     
     # RGB
     rgb_picture = picture.convert('RGB');
     
-    # Save as .jpg, temporally, as .png is a lossless format
+    # Save a resized version as .jpg, temporally, as .png is a lossless format
     tmp_path = '/tmp/tmp_compressed_image.jpg';
-    rgb_picture.save(tmp_path, optimize=True, quality=quality);
+    width, height = rgb_picture.size;
+    new_dimensions = (int(width*scale_factor), int(height*scale_factor));
+    rgb_picture.resize(new_dimensions, Image.LANCZOS).save(tmp_path, optimize=True, quality=quality);
     
+    # Get the size in Bytes of the original file before replacing
+    original_image_size = os.path.getsize(image_path);
+        
     # Convert the temporal image to the correct format and remove it!
     tmp_picture = Image.open(tmp_path);
     tmp_picture.save(compressed_image_path);
     os.remove(tmp_path);
     
-    # Get the size of both images in Bytes
-    original_image_size = os.path.getsize(image_path);
+    # Get the size of the new image too
     compressed_image_size = os.path.getsize(compressed_image_path);
     
     # Compression difference
     saving_diff = compressed_image_size - original_image_size;
-    print(f"[+] Image size change: {saving_diff/original_image_size*100:.2f}% of the original image size.");
+    print(f'[+] Image size change: {saving_diff/original_image_size*100:.2f}% of the original image size.');
     
   
 """
-> python3 compressor.py images_dir quality should_replace
+> python3 compressor.py images_dir quality scale_factor should_replace
 """
 def main():
     # Read args
     images_dir = sys.argv[1];
     quality = int(sys.argv[2]);
-    should_replace = sys.argv[3].lower() == 'true';
-
+    scale_factor = float(sys.argv[3]);
+    should_replace = sys.argv[4].lower() == 'true';
+    
+    
     # Supported extensions
-    supported_extensions = [".jpg", ".jpeg", ".png"];
+    supported_extensions = ['.jpg', '.jpeg', '.png'];
 
     # Get the current directory
     cwd = os.getcwd();
@@ -67,8 +73,8 @@ def main():
             else:
                 out_path = f'{images_dir}/{filename}';
 
-            compress_image(image_path, out_path, quality);
+            compress_image(image_path, out_path, quality, scale_factor);
 
 
-if (__name__ == "__main__"):
+if (__name__ == '__main__'):
     main();
